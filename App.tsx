f294f044bc93b865b551adf46cac0402f8b2dd7f
Sandra Chatphatthanasiri <sakisandra.c@gmail.com>
2023-11-22 13:35:03 -0700

    Add top ten scoreboard to main view

--- App.tsx
+++ App.tsx
@@ -7,40 +7,52 @@ import ScoreCard from './src/components/ScoreCard';
 
 export default function App() {
   const [searchName, setSearchName] = useState<string>("");
-  const [displayInfo, setDisplayInfo] = useState<string>("laila");
+  const [displayInfo, setDisplayInfo] = useState<string>("");
 
   const renderText = (data: DataSet): React.ReactElement[] => {
     const dataArray = Object.values(data);
-    
+
     return dataArray
-    .filter((user: UserType) => { 
+      .filter((user: UserType) => {
         return user.name.toLowerCase() === displayInfo.toLowerCase()
-    })
-    .map((user: UserType) => (
-      <ScoreCard name={user.name} bananas={user.bananas}/>
+      })
+      .map((user: UserType) => (
+        <ScoreCard key={user.uid} name={user.name} rank={dataArray.indexOf(user)+1} bananas={user.bananas} />
+      ));
+
+  };
+
+  const renderTopTen = (data: DataSet): React.ReactElement[] => {
+    const dataArray = Object.values(data);
+
+    dataArray.sort((a: UserType, b: UserType) => b.bananas - a.bananas);
+  
+    return dataArray.slice(0, 10).map((user: UserType) => (
+      <ScoreCard key={user.uid} name={user.name} rank={dataArray.indexOf(user)+1} bananas={user.bananas} />
     ));
-    
   };
 
   const handlePress = () => {
-    console.log(searchName)
+    setDisplayInfo(searchName)
   }
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.textInput}
-        placeholder='username'
-        onChangeText={(text) => setSearchName(text)}
-      />
-      <TouchableOpacity
-        style={styles.button}
-      onPress={handlePress}
-      >
-        <Text style={styles.buttonText}>Search</Text>
-      </TouchableOpacity>
-      {renderText(data)}
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder='username'
+          onChangeText={(text) => setSearchName(text)}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handlePress}
+        >
+          <Text style={styles.buttonText}>Search</Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
+      {renderTopTen(data)}
     </View>
   );
 }
@@ -48,20 +60,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: 400,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column'
+  },
+  inputContainer: {
+    width: 400,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row'
   },
   textInput: {
     borderColor: '#000000',
     height: 50,
-    width: 200,
+    width: 100,
     borderWidth: 1,
   },
   button: {
     borderColor: '#000000',
     height: 50,
-    width: 200,
+    width: 100,
     borderWidth: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
--- src/components/ScoreCard.tsx
+++ src/components/ScoreCard.tsx
@@ -1,16 +1,28 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 
 interface ScoreCardProps {
   name: string,
-  bananas: number
+  bananas: number,
+  rank: number
 }
 
-export default function ScoreCard({name, bananas}: ScoreCardProps) {
+export default function ScoreCard({name, bananas, rank}: ScoreCardProps) {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>{name}</Text>
       <Text>{bananas}</Text>
+      <Text>{rank}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: 400,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row'
+  },
+})
