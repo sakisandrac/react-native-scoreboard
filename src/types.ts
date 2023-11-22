2af2c536d3d24522ebdf8eb2fbea9c6bd541a27b
Sandra Chatphatthanasiri <sakisandra.c@gmail.com>
2023-11-22 13:12:38 -0700

    Create types for data

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type UserType = {
+    "bananas": number,
+    "lastDayPlayed": string,
+    "longestStreak": number,
+    "name": string,
+    "stars": number,
+    "subscribed": boolean,
+    "uid": string
+}
+
+export type DataSet = {
+    [key: string] : UserType
+}
