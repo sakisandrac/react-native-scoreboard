89299792478e67719b721c3458a48cb6f42f3a21
Sandra Chatphatthanasiri <sakisandra.c@gmail.com>
2023-11-30 19:36:02 -0700

    Add test for createDataObject util

--- src/test/testData.ts
+++ src/test/testData.ts
@@ -1,143 +1,143 @@
-export const testData = 
+export const testData =
 {
-  "1" : {
-    "bananas" : 200,
-    "lastDayPlayed" : "2018-11-22",
-    "longestStreak" : 1,
-    "name" : "Harry Potter",
-    "stars" : 6,
-    "subscribed" : false,
-    "uid" : "1"
+  "1": {
+    "bananas": 200,
+    "lastDayPlayed": "2018-11-22",
+    "longestStreak": 1,
+    "name": "Harry Potter",
+    "stars": 6,
+    "subscribed": false,
+    "uid": "1"
   },
-  "2" : {
-    "bananas" : 100,
-    "lastDayPlayed" : "2017-11-01",
-    "longestStreak" : 0,
-    "name" : "Ron Weasly",
-    "stars" : 4,
-    "subscribed" : false,
-    "uid" : "2"
+  "2": {
+    "bananas": 100,
+    "lastDayPlayed": "2017-11-01",
+    "longestStreak": 0,
+    "name": "Ron Weasly",
+    "stars": 4,
+    "subscribed": false,
+    "uid": "2"
   },
-  "3" : {
-    "bananas" : 300,
-    "lastDayPlayed" : "2018-10-17",
-    "longestStreak" : 1,
-    "name" : "Hermione Granger",
-    "stars" : 4,
-    "subscribed" : false,
-    "uid" : "3"
+  "3": {
+    "bananas": 300,
+    "lastDayPlayed": "2018-10-17",
+    "longestStreak": 1,
+    "name": "Hermione Granger",
+    "stars": 4,
+    "subscribed": false,
+    "uid": "3"
   },
-  "4" : {
-    "bananas" : 0,
-    "lastDayPlayed" : "2017-11-01",
-    "longestStreak" : 0,
-    "name" : "Yo Yo Chou",
-    "stars" : 4,
-    "subscribed" : false,
-    "uid" : "4"
+  "4": {
+    "bananas": 0,
+    "lastDayPlayed": "2017-11-01",
+    "longestStreak": 0,
+    "name": "Yo Yo Chou",
+    "stars": 4,
+    "subscribed": false,
+    "uid": "4"
   },
-  "5" : {
-    "bananas" : 0,
-    "lastDayPlayed" : "2017-11-01",
-    "longestStreak" : 0,
-    "name" : "Toke Chen",
-    "stars" : 4,
-    "subscribed" : false,
-    "uid" : "5"
+  "5": {
+    "bananas": 0,
+    "lastDayPlayed": "2017-11-01",
+    "longestStreak": 0,
+    "name": "Toke Chen",
+    "stars": 4,
+    "subscribed": false,
+    "uid": "5"
   },
-  "6" : {
-    "bananas" : 20,
-    "lastDayPlayed" : "2017-11-01",
-    "longestStreak" : 0,
-    "name" : "Ryan",
-    "stars" : 5,
-    "subscribed" : false,
-    "uid" : "6"
+  "6": {
+    "bananas": 20,
+    "lastDayPlayed": "2017-11-01",
+    "longestStreak": 0,
+    "name": "Ryan",
+    "stars": 5,
+    "subscribed": false,
+    "uid": "6"
   },
-  "7" : {
-    "bananas" : 30,
-    "lastDayPlayed" : "2018-08-04",
-    "longestStreak" : 1,
-    "name" : "Amy",
-    "stars" : 9,
-    "subscribed" : false,
-    "uid" : "7"
+  "7": {
+    "bananas": 30,
+    "lastDayPlayed": "2018-08-04",
+    "longestStreak": 1,
+    "name": "Amy",
+    "stars": 9,
+    "subscribed": false,
+    "uid": "7"
   },
-  "8" : {
-    "bananas" : 40,
-    "lastDayPlayed" : "2018-12-10",
-    "longestStreak" : 2,
-    "name" : "Mark Wong",
-    "stars" : 7,
-    "subscribed" : false,
-    "uid" : "8"
+  "8": {
+    "bananas": 40,
+    "lastDayPlayed": "2018-12-10",
+    "longestStreak": 2,
+    "name": "Mark Wong",
+    "stars": 7,
+    "subscribed": false,
+    "uid": "8"
   },
-  "9" : {
-    "bananas" : 50,
-    "lastDayPlayed" : "2018-06-17",
-    "longestStreak" : 1,
-    "name" : "Happy",
-    "stars" : 5,
-    "subscribed" : false,
-    "uid" : "9"
+  "9": {
+    "bananas": 50,
+    "lastDayPlayed": "2018-06-17",
+    "longestStreak": 1,
+    "name": "Happy",
+    "stars": 5,
+    "subscribed": false,
+    "uid": "9"
   },
-  "10" : {
-    "bananas" : 60,
-    "lastDayPlayed" : "2017-11-01",
-    "longestStreak" : 0,
-    "name" : "Bob Burgers",
-    "stars" : 5,
-    "subscribed" : false,
-    "uid" : "10"
+  "10": {
+    "bananas": 60,
+    "lastDayPlayed": "2017-11-01",
+    "longestStreak": 0,
+    "name": "Bob Burgers",
+    "stars": 5,
+    "subscribed": false,
+    "uid": "10"
   },
-  "12" : {
-    "bananas" : 80,
-    "lastDayPlayed" : "2019-01-24",
-    "longestStreak" : 1,
-    "name" : "Bo",
-    "stars" : 5,
-    "subscribed" : false,
-    "uid" : "12"
+  "12": {
+    "bananas": 80,
+    "lastDayPlayed": "2019-01-24",
+    "longestStreak": 1,
+    "name": "Bo",
+    "stars": 5,
+    "subscribed": false,
+    "uid": "12"
   },
-  "11" : {
-    "bananas" : 90,
-    "lastDayPlayed" : "2019-01-24",
-    "longestStreak" : 1,
-    "name" : "",
-    "stars" : 5,
-    "subscribed" : false,
-    "uid" : "11"
+  "11": {
+    "bananas": 90,
+    "lastDayPlayed": "2019-01-24",
+    "longestStreak": 1,
+    "name": "",
+    "stars": 5,
+    "subscribed": false,
+    "uid": "11"
   }
 }
-export const testMongoData = {
-  "10" : {
-    "bananas" : 60,
-    "lastDayPlayed" : "2017-11-01",
-    "longestStreak" : 0,
-    "name" : "Bob Burgers",
-    "stars" : 5,
-    "subscribed" : false,
-    "uid" : "10",
+export const testMongoData = [
+  {
+    "bananas": 60,
+    "lastDayPlayed": "2017-11-01",
+    "longestStreak": 0,
+    "name": "Bob Burgers",
+    "stars": 5,
+    "subscribed": false,
+    "uid": "10",
     "_id": "1212"
   },
-  "12" : {
-    "bananas" : 80,
-    "lastDayPlayed" : "2019-01-24",
-    "longestStreak" : 1,
-    "name" : "Bo",
-    "stars" : 5,
-    "subscribed" : false,
-    "uid" : "12",
+  {
+    "bananas": 80,
+    "lastDayPlayed": "2019-01-24",
+    "longestStreak": 1,
+    "name": "Bo",
+    "stars": 5,
+    "subscribed": false,
+    "uid": "12",
     "_id": "1234"
   },
-  "11" : {
-    "bananas" : 90,
-    "lastDayPlayed" : "2019-01-24",
-    "longestStreak" : 1,
-    "name" : "",
-    "stars" : 5,
-    "subscribed" : false,
-    "uid" : "11",
+  {
+    "bananas": 90,
+    "lastDayPlayed": "2019-01-24",
+    "longestStreak": 1,
+    "name": "",
+    "stars": 5,
+    "subscribed": false,
+    "uid": "11",
     "_id": "1233"
   }
-}
+]
